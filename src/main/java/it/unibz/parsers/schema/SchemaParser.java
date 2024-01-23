da4542df1a3f98ec0b45fa2c8283a089d4ac9d0d
Elias Binder <ebinder@unibz.it>
2024-01-23 21:12:59 +0100

    Refactoring: Out-of-scope parsing algorithm

--- src/main/java/it/unibz/parsers/schema/SchemaParser.java
+++ src/main/java/it/unibz/parsers/schema/SchemaParser.java
@@ -42,56 +42,7 @@ public class SchemaParser implements SchemaParsable {
     public void parse(String name, ObjectNode objectNode) {
 
         if (objectNode == null) { //Only parse by name in a possibly completely different scope
-            System.out.println("Starting out-of-scope parsing of " + name);
-
-            //Simulate Stack for out-of-scope parsing
-            SchemaParserStack _parserStack = parserStack;
-            SchemaParserStack simulatedStack = new SchemaParserStack();
-            parserStack = simulatedStack;
-
-            String[] refParts = name.split("/");
-
-            //Get ref node from json schema
-            ObjectNode refNode = schema;
-            for (int i = 0; i < refParts.length; i++) {
-                refNode = (ObjectNode) refNode.get(refParts[i]);
-            }
-
-            //Prepare simulated stack
-            if (refParts.length > 1) {
-                StringBuilder currentScope = new StringBuilder();
-                for (int i = 0; i < refParts.length; i++) {
-                    if (i > 0)
-                        currentScope.append("/" + refParts[i]);
-                    else
-                        currentScope.append(refParts[i]);
-
-                    if (i == 0) {
-                        EntityType root = aom.getEntityType(refParts[i]);
-                        if (root == null) {
-                            parse(refParts[i], null);
-                            root = aom.getEntityType(refParts[i]);
-                        }
-                        simulatedStack.push(root);
-                    } else {
-                        EntityType current = aom.getEntityType(currentScope.toString());
-                        if (current == null) {
-                            parse(currentScope.toString(), null);
-                            current = aom.getEntityType(currentScope.toString());
-                        }
-                        simulatedStack.push(simulatedStack.peek().getAccountabilityType(refParts[i]).getAccountedType());
-                    }
-                }
-            }
-            System.out.print("Simulated stack: ");
-            simulatedStack.debug();
-
-            //Parse
-            parse(name, refNode);
-
-            //Restore stack
-            parserStack = _parserStack;
-
+            parseOutOfScope(name);
             return;
         }
 
@@ -105,8 +56,50 @@ public class SchemaParser implements SchemaParsable {
             arrayParser.parse(name, objectNode);
         } else if (objectNode.has("type")) {
             propertyParser.parse(name, objectNode);
+        } else {
+            System.out.println("Failed to parse " + name + " (unknown type)");
         }
     }
+
+
+    public void parseOutOfScope(String name) {
+        System.out.println("Starting out-of-scope parsing of " + name);
+
+        //Simulate Stack for out-of-scope parsing. Backup current stack and restore it after parsing
+        SchemaParserStack _parserStack = parserStack;
+        SchemaParserStack simulatedStack = new SchemaParserStack();
+        parserStack = simulatedStack;
+
+        String[] nameParts = name.split("/");
+        if (nameParts.length == 0) nameParts = new String[]{name};
+
+        //Parse parent schema components first until getting to the ref node
+
+        ObjectNode currentNode = schema;
+        StringBuilder currentScope = new StringBuilder();
+
+
+        for (int i = 0; i < nameParts.length; i++) {
+            //Set currentNode and currentScope
+            currentNode = (ObjectNode) currentNode.get(nameParts[i]);
+            if (i > 0) currentScope.append("/" + nameParts[i]);
+            else currentScope.append(nameParts[i]);
+
+            //Parse currentNode
+            EntityType current = aom.getEntityType(currentScope.toString());
+            if (current == null) {
+                parse(currentScope.toString(), currentNode);
+                current = aom.getEntityType(currentScope.toString());
+            }
+
+            if (i == 0) simulatedStack.push(current);
+            else simulatedStack.push(simulatedStack.peek().getAccountabilityType(nameParts[i]).getAccountedType());
+        }
+
+        //Restore stack
+        parserStack = _parserStack;
+    }
+
 
     public Aom getAom() {
         return aom;
